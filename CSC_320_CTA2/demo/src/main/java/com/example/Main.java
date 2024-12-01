437136ab2f935c4a06ab4d54debb841aeacf9502
Ivory <ibrallier55@gmail.com>
2024-12-01 13:15:13 -0700

    //Commit for CSC_320_CTA2

--- CSC_320_CTA2/demo/src/main/java/com/example/Main.java
+++ CSC_320_CTA2/demo/src/main/java/com/example/Main.java
@@ -1,8 +1,33 @@
 package com.example;
-
+import java.util.Scanner;
 public class Main {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    double IncomeUnder500TaxRate = 0.10;
+    double IncomeAt1500TaxRate = 0.15;
+    double IncomeAt2500TaxRate = 0.20;
+    double IncomeOver2500TaxRate = 0.30;
+    double Tax = 0;
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Weekly Income: ");
+    double income = scanner.nextDouble();
+    scanner.close();
+
+    if (income < 500){
+        Tax = IncomeUnder500TaxRate;
+    }
+    else if(income >= 500 && income < 1500){
+        Tax = IncomeAt1500TaxRate;
+    }
+    else if(income >= 1500 && income < 2500){
+        Tax = IncomeAt2500TaxRate;
+    }
+    else if(income >= 2500){
+        Tax = IncomeOver2500TaxRate;
+    }
+
+    System.out.print("You Tax Rate is " + Tax);
+
     }
 }
