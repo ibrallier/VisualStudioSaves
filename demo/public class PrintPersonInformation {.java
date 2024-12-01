3a10d154a8516374022a5ed60f8720976bab07fe
Ivory <ibrallier55@gmail.com>
2024-12-01 12:13:14 -0700

    //Still FIguring out these git repositories

---
+++ demo/public class PrintPersonInformation {.java
@@ -0,0 +1,17 @@
+public class PrintPersonInformation {
+    public static void main(String[] args) {
+        // Declare variables
+        String firstName = "Amit";
+        String lastName = "Bisht";
+        String streetAddress = "123 Street";
+        String city = "SRE";
+        String zipCode = "10001";
+
+        // Print information
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
